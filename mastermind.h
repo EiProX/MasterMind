0250ddb72a4d133e9335b4fbf03b94d5dff6895d
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-03 18:23:51 +0100

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -4,3 +4,9 @@
 * Créé le : 03/12/2024 par EiProX
 * Mis à jour le : 03/12/2024 par EiProX
 *********************************************************/
+
+#ifndef mastermind_h
+#define mastermind_h
+
+
+#endif /* MY_CLASS_H */
