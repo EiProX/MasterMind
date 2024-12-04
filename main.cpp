282c4bc7ae17642222793d4b621b1023ace97e85
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-04 13:20:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,11 @@
 * Créé le : 03/12/2024 par EiProX
 * Mis à jour le : 03/12/2024 par EiProX
 *********************************************************/
+
+#include <mastermind.h>
+
+int main()
+{
+    afficherMenu();
+    return 0;
+}
