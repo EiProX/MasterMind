3c9f377d7be2a0a8f1d223fe3588fda805aea7df
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-04 14:11:56 +0100

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -19,11 +19,7 @@ const int LONGUEUR_COMBINAISON = 5; // Longueur de la combinaison secrète.
 const int TENTATIVES_MAX = 10; // Nombre maximum de tentatives.
 const char COULEURS_DISPONIBLES[6] = {'R', 'V', 'B', 'J', 'O', 'P'};
 
-char combinaisonSecrete[LONGUEUR_COMBINAISON]; // Tableau contenant la combinaison générée.
-char tentative[LONGUEUR_COMBINAISON]; // Tableau pour stocker la tentative du joueur.
-int bienPlaces; // Compteur des couleurs bien placées.
-int malPlaces; // Compteur des couleurs mal placées.
-int tentativesRestantes; // Nombre de tentatives restantes.
+
 
 void afficherMenu();
 void afficherRegles();
