303c1453b5d9207448fdb8e2288b764aa036bb8f
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-04 13:56:42 +0100

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -15,10 +15,6 @@
 
 using namespace std;
 
-void afficherRegles();
-void afficherMenu();
-
-
 const int LONGUEUR_COMBINAISON = 5; // Longueur de la combinaison secrète.
 const int TENTATIVES_MAX = 10; // Nombre maximum de tentatives.
 const char COULEURS_DISPONIBLES[6] = {'R', 'V', 'B', 'J', 'O', 'P'};
@@ -28,6 +24,14 @@ char tentative[LONGUEUR_COMBINAISON]; // Tableau pour stocker la tentative du jo
 int bienPlaces; // Compteur des couleurs bien placées.
 int malPlaces; // Compteur des couleurs mal placées.
 int tentativesRestantes; // Nombre de tentatives restantes.
+
+void afficherMenu();
+void afficherRegles();
+void jouerPartie();
+void genererCombinaison(char combinaisonSecrete[]);
+void lireTentative(char tentative[]);
+void calculerIndices(const char combinaisonSecrete[], const char tentative[], int& bienPlaces, int& malPlaces);
+void afficherIndices(int bienPlaces, int malPlaces);
 
 
 
