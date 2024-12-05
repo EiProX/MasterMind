64124b2b4e6a3cb1ebed1134706a011510d41213
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-05 16:41:57 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,29 @@
 /*********************************************************
 * Nom du projet :     MasterMind
-* Nom du fichier : main.cpp
+* Nom du fichier : fonction.cpp
 * Créé le : 03/12/2024 par EiProX
 * Mis à jour le : 03/12/2024 par EiProX
 *********************************************************/
 
 #include <mastermind.h>
 
-int main()
-{
+int main() {
+    bool rejouer = true;
+    while (rejouer) {
+        afficherMenu();
+        int choix = obtenirChoixMenu();
+
+        if (choix == 1) {
+            jouerPartie();
+            rejouer = demanderRejouer();
+        } else if (choix == 2) {
+            afficherRegles();
+        } else if (choix == 3) {
+            cout << "Merci d'avoir joué. Au revoir !" << endl;
+            rejouer = false;
+        } else {
+            cout << "Choix invalide. Veuillez réessayer." << endl;
+        }
+    }
     return 0;
 }
