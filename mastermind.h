3346d180a381cb4270bc10cc928321d7dbf8d5e4
EiProX <108833428+EiProX@users.noreply.github.com>
2024-12-17 15:01:10 +0100

    Update mastermind.h

--- mastermind.h
+++ mastermind.h
@@ -2,7 +2,7 @@
 * Nom du projet :     MasterMind
 * Nom du fichier : fonction.cpp
 * Créé le : 03/12/2024 par EiProX
-* Mis à jour le : 03/12/2024 par EiProX
+* Mis à jour le : 16/12/2024 par EiProX
 *********************************************************/
 
 #ifndef MASTERMIND_H
